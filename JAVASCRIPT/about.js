7dbddf5df4f167186d7b3dbf9febf486227cfaec
Chukwunenye Moses <chunkums088@gmail.com>
2022-12-23 10:59:02 +0100

    Create about.js

---
+++ JAVASCRIPT/about.js
@@ -0,0 +1,28 @@
+      let mybutton = document.getElementById("gototop");
+      let mybutton2 = document.getElementById("gototop2");
+
+      window.onscroll = function() {
+        scrollFunction();
+      };
+
+      function scrollFunction() {
+        if (document.body.scrollTop > 100 && screen.width > 500) {
+          mybutton.style.display = "block";
+        } else if(screen.width < 500){
+          mybutton2.style.display = "block";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+      }
+
+      function open() {
+        let menulists = document.querySelectorAll(".menulist");
+
+        console.log(menulists);
+
+        menulists.forEach(menulist => menulist.classList.toggle("appear"));
+      }
+
+      document.querySelector("#logo2").addEventListener("click", open);
